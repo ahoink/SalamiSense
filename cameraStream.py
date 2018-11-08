11f5b0a35a2748376d8ccd913c35ffaca5c85f99
ahoink <ajh345@drexel.edu>
2018-11-08 15:11:18 -0500

    realsense camera stream

---
+++ cameraStream.py
@@ -0,0 +1,35 @@
+import pyrealsense2 as rs
+
+pipeline = rs.pipeline()
+
+# Enable color and depth streams
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 15)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+
+# Start streams
+profile = pipeline.start(config)
+
+# Depth sensor info
+depthSensor = profile.get_device().first_depth_sensor()
+depthScale = depthSensor.get_depth_scale()
+
+# Enable stream alignment to align depth to color
+alignStream = rs.stream.color
+align = rs.align(alignStream)
+
+while True:
+	# Get next set of frames from stream
+	frames = pipeline.wait_for_frames()
+
+	# Align depth frame to color frame
+	alignedFrames = align.process(frames)
+
+	# Get aligned depth and color frames
+	depthFrame = alignedFrames.get_depth_frame()
+	colorFrame = alignedFrames.get_color_frame()
+
+	
+	
+# Stop streams
+pipeline.stop()

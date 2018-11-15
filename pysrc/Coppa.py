89dc5bbb52e41336780b7830f0b84e631adccef3
dwong23 <dcwong96@gmail.com>
2018-11-15 14:28:40 -0500

    Coppa Encoder (Rev 0.0)

---
+++ pysrc/Coppa.py
@@ -0,0 +1,15 @@
+import base64
+import numpy as np
+import cv2 as cv
+
+def encodeColorFrame(colorFrame):
+	r = np.asanyarray(colorFrame.get_data())
+	ret, buf = cv.imencode('.jpg', color)
+	jpgTxt = base64.b64encode(buf)
+	return jpgTxt
+
+def decodeColorFrame(colorTxt):
+	imgData = base64.b64decode(colorTxt)
+	imgData = np.frombuffer(imgData, dtype=np.uint8)
+	img = cv.imdecode(imgData, flags=1)
+	return img

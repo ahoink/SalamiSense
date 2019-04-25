648cb7dcb1f8a4066c471c3082cfd0e5198e245b
dwong23 <dcwong96@gmail.com>
2019-04-25 16:25:35 -0400

    rk depth vertices calc

--- rksrc/rkDemo0.py
+++ rksrc/rkDemo0.py
@@ -6,28 +6,33 @@ from PIL import Image
 import base64
 import cv2 as cv
 import time
+import pickle
 
 DEBUG = True
 
 def main():
 	print("Enabling streams and starting camera...")
 	pipeline = rs.pipeline()
+	pc = rs.pointcloud()
 
 	# Enable color and depth streams
 	config = rs.config()
-	config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
-	config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
+	config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+	config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 	# Start streams
 	profile = pipeline.start(config)
 
 	# Depth sensor info
 	depthSensor = profile.get_device().first_depth_sensor()
+	
+	depthProfile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+	#epthSensor.set_option(rs.option.visual_preset, 0)
 	depthScale = depthSensor.get_depth_scale()
-
+	
 	# Enable stream alignment to align depth to color
-	alignStream = rs.stream.color
-	align = rs.align(alignStream)
+	#alignStream = rs.stream.color
+	#align = rs.align(alignStream)
 
 	print("Initializing Client")
 	client = Roni.RoniClient()
@@ -38,22 +43,32 @@ def main():
 	while True:
 		# Get next set of frames from stream
 		frames = pipeline.wait_for_frames()
-
+		
+		depthFrame = frames.get_depth_frame()
+		colorFrame = frames.get_color_frame()
+		
 		# Align depth frame to color frame
-		alignedFrames = align.process(frames)
-
+		pc.map_to(colorFrame)
+		#alignedFrames = align.process(frames)
+		
+		pts = pc.calculate(depthFrame)
+		v = pts.get_vertices()
+		verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+		
+		vertPick = pickle.dumps(verts, protocol=pickle.HIGHEST_PROTOCOL)
+		
 		# Get aligned depth and color frames
-		depthFrame = alignedFrames.get_depth_frame()
-		colorFrame = alignedFrames.get_color_frame()
-
-		#print(len(jpgTxt))
+		#depthFrame = alignedFrames.get_depth_frame()
+		#colorFrame = alignedFrames.get_color_frame()
+		
 		depthTxt = Coppa.encodeDepthFrame(depthFrame)
 		colorTxt = Coppa.encodeColorFrame(colorFrame)
 		
-		#if client.sendData(depthTxt) == False and client.sendData(colorTxt) == False:
-		#	break
 		client.sendData(depthTxt, Roni.TYPE_DEPTH)
-		client.sendData(colorTxt, Roni.TYPE_RGB)
+		#client.sendData(vertPick, Roni.TYPE_3D)
+		if not client.sendData(colorTxt, Roni.TYPE_RGB):
+			break
+		
 
 	client.close()
 

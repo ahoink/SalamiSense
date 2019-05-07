0d14a555e551aea3d1a6e8605d810e2bdc568aab
dwong23 <dcwong96@gmail.com>
2019-05-07 12:56:38 -0400

    Sopressata pyserial, rk-Cape UART

--- rksrc/rkDemo0.py
+++ rksrc/rkDemo0.py
@@ -1,9 +1,9 @@
 import pysrc.Roni as Roni
 import pysrc.Coppa as Coppa
+import pysrc.Sopressata as Sopressata
 import pyrealsense2 as rs
 import numpy as np
 from PIL import Image
-import base64
 import cv2 as cv
 import time
 import pickle
@@ -14,6 +14,12 @@ def main():
 	print("Enabling streams and starting camera...")
 	pipeline = rs.pipeline()
 	pc = rs.pointcloud()
+
+	uartPort = "/dev/ttyS3"
+	uartBaud = 115200
+	rtscts = True
+
+	cape = Sopressata.Cape(uartPort, uartBaud, rtscts)
 
 	# Enable color and depth streams
 	config = rs.config()
@@ -38,6 +44,8 @@ def main():
 	client = Roni.RoniClient()
 	print("Connecting...")
 	client.connect()
+	cape.openPort()
+	print("UART connected to cape:", uartPort)
 	print("streaming...")
 
 	while True:
@@ -61,6 +69,14 @@ def main():
 		#depthFrame = alignedFrames.get_depth_frame()
 		#colorFrame = alignedFrames.get_color_frame()
 		
+		# Read sensor data from Cape UART
+		rawRead = cape.readSerial()
+		if rawRead:
+			co2, tvoc, temp, humid = cape.getAllSensorData(rawRead)
+			sensors = [co2, tvoc, temp, humid]
+			senseDump = pickle.dumps(sensors, protocol=pickle.HIGHEST_PROTOCOL)
+			client.sendData(senseDump, Roni.TYPE_EDGE)
+
 		depthTxt = Coppa.encodeDepthFrame(depthFrame)
 		colorTxt = Coppa.encodeColorFrame(colorFrame)
 		
@@ -71,6 +87,7 @@ def main():
 		
 
 	client.close()
+	cape.closePort()
 
 if __name__ == '__main__':
 	main()

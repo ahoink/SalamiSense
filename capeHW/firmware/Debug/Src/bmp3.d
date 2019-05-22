ca2717df010dc84337b18b66082e68980216a07a
dwong23 <dcwong96@gmail.com>
2019-05-22 12:42:09 -0400

    removed generated STM files; leaving src, includes

--- capeHW/firmware/Debug/Src/bmp3.d
+++
@@ -1,7 +0,0 @@
-Src/bmp3.o: ../Src/bmp3.c \
- M:/OneDrive/Documents/Drexel/Senior\ Design/Rock960Cape/STM32/firmware/Inc/bmp3.h \
- M:/OneDrive/Documents/Drexel/Senior\ Design/Rock960Cape/STM32/firmware/Inc/bmp3_defs.h
-
-M:/OneDrive/Documents/Drexel/Senior\ Design/Rock960Cape/STM32/firmware/Inc/bmp3.h:
-
-M:/OneDrive/Documents/Drexel/Senior\ Design/Rock960Cape/STM32/firmware/Inc/bmp3_defs.h:
--- capeHW/firmware/Debug/Src/lsm303agr_reg.d
+++
@@ -1,4 +0,0 @@
-Src/lsm303agr_reg.o: ../Src/lsm303agr_reg.c \
- M:/OneDrive/Documents/Drexel/Senior\ Design/Rock960Cape/STM32/firmware/Inc/lsm303agr_reg.h
-
-M:/OneDrive/Documents/Drexel/Senior\ Design/Rock960Cape/STM32/firmware/Inc/lsm303agr_reg.h:
